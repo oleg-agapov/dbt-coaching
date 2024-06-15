09e8cb9fa6a61b8a73e81db3258bf3ca316dec20
oleg-agapov <oleg-agapov@users.noreply.github.com>
2024-06-15 19:34:35 +0000

    Revert "Add practice 3"

--- my_dbt_project/macros/truncate_to_daty.sql
+++
@@ -1,5 +0,0 @@
-{% macro truncate_to_day(col) -%}
-    
-date_trunc('day', {{ col }})
-
-{%- endmacro %}
--- my_dbt_project/models/marts/fact_daily_trips.sql
+++ my_dbt_project/models/marts/fact_daily_trips.sql
@@ -1,8 +1,7 @@
 select
-    {{ truncate_to_day('started_at') }} as trip_date,
+    date_trunc('day', started_at) as trip_date,
     rideable_type,
     member_casual,
     count(ride_id) as trips
 from {{ ref('stg_bike_trips') }}
-where started_at between '{{ var("date_from", "2024-01-01") }}' and '{{ var("date_to", "2024-01-31") }}'
 group by all
