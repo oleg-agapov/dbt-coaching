95c1a0df1ff7486f04227102ef87b967db654171
oleg-agapov <oleg-agapov@users.noreply.github.com>
2024-06-15 19:02:04 +0000

    Add practice 3

--- my_dbt_project/models/marts/fact_daily_trips.sql
+++ my_dbt_project/models/marts/fact_daily_trips.sql
@@ -1,7 +1,8 @@
 select
-    date_trunc('day', started_at) as trip_date,
+    {{ truncate_to_day('started_at') }} as trip_date,
     rideable_type,
     member_casual,
     count(ride_id) as trips
 from {{ ref('stg_bike_trips') }}
+where started_at between '{{ var("date_from", "2024-01-01") }}' and '{{ var("date_to", "2024-01-31") }}'
 group by all
